00e65ee464e32629b464d0b6c483784e44e8078c
tjr16 <jerryaccount@126.com>
2020-12-14 14:03:54 +0100

    Graph class

---
+++ workspace/Graph.m
@@ -0,0 +1,19 @@
+classdef Graph
+    
+    % -- properties
+    properties
+%         Value {mustBeNumeric}
+    end
+    
+    % -- methods
+    methods
+%     	function r = roundOff(obj)
+%             r = round([obj.Value],2);
+%         end
+%         
+%         function r = multiplyBy(obj,n)
+%             r = [obj.Value] * n;
+%         end
+    end
+    
+end
