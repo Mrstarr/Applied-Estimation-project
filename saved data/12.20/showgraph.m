8c101b56ac29948097f642b7288864318adbe030
Yiming97 <42455721+Mrstarr@users.noreply.github.com>
2021-03-08 20:24:29 +0100

    Delete saved data/12.20 directory

--- saved data/12.20/showgraph.m
+++
@@ -1,8 +0,0 @@
-function showgraph()
-    x = load("state_1.mat","-mat");
-    m = load("landmark_1.mat","-mat");
-    hold on
-%     x0 = [-67.6493; -41.7142; 35.5*pi/180];
-    plot(x.state(1,:),x.state(2,:),'k');
-    plot(m.landmark(1,:),m.landmark(2,:),'b.','MarkerSize', 5);
-end
